f8b48a7cdcb804f1c176db4f43228326763d85a8
Rohita2002 <sairohita30032002@gmail.com>
2023-05-02 03:28:39 +0530

    first commit

---
+++ src/components/Admin/Admin.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import MainDash from '../MainDash/MainDash.jsx';
+import RightSide from '../RigtSide/RightSide';
+import Sidebar from '../Sidebar.jsx';
+
+class Admin extends Component {
+	constructor(props) {
+		super(props);
+
+		// window.location.reload();
+	}
+
+	// componentDidMount() {
+	// 	window.location.reload();
+	// }
+
+	render() {
+		return (
+			<div className="App">
+				<div className="AppGlass">
+					<Sidebar />
+					<MainDash />
+					<RightSide />
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Admin;
